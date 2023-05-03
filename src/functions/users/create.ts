3e63623455c2e659f19a56a18254038a7e356cd3
Pengpa0012 <peng.ilikha@gmail.com>
2023-05-03 20:47:42 +0800

    update create handler

--- src/functions/users/create.ts
+++ src/functions/users/create.ts
@@ -1,19 +1,41 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const tableName = "your-dynamodb-table-name"; 
 // Create CRUD todo
 // Save on dynamo DB
 
 export const createTodo = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log(event)
+  try {
+    const { title, description } = JSON.parse(event.body as string);
+    
+    const item = {
+      id: Math.random().toString(36).substr(2, 9), // generate a random id
+      title,
+      description
+    };
+    
+    await dynamoDB.put({
+      TableName: tableName,
+      Item: item
+    }).promise();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        event
-      },
-      null,
-      2
-    ),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Todo created successfully",
+        todo: item
+      })
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "An error occurred",
+        error
+      })
+    };
+  }
 };
