41cc4910f2bd86973d379d682d61e21c1738dc5e
Pengpa0012 <peng.ilikha@gmail.com>
2023-05-05 08:55:22 +0800

    update todo update/delete handler

--- src/functions/users/delete.ts
+++ src/functions/users/delete.ts
@@ -1,15 +1,55 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const tableName = "Todos";
 
 export const deleteTodo = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        event
-      },
-      null,
-      2
-    ),
+  const id = event.pathParameters?.id;
+
+  if (!id) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          message: "Missing todo ID"
+        },
+        null,
+        2
+      )
+    };
+  }
+
+  const params = {
+    TableName: tableName,
+    Key: {
+      id: id
+    }
   };
+
+  try {
+    const data = await dynamoDB.delete(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Todo deleted successfully",
+          deletedTodo: data.Attributes
+        },
+        null,
+        2
+      ),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          message: "Cannot delete todo"
+        },
+        null,
+        2
+      ),
+    };
+  }
 };
--- src/functions/users/update.ts
+++ src/functions/users/update.ts
@@ -1,16 +1,49 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import AWS from "aws-sdk";
 
-export const updateTodo = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const tableName = "Todos";
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        event
-      },
-      null,
-      2
-    ),
+export const updateTodo = async (event: APIGatewayProxyEvent) => {
+  const id = event.pathParameters?.id;
+  const { title, description } = JSON.parse(event.body || "{}");
+
+  const params = {
+    TableName: tableName,
+    Key: {
+      id: id
+    },
+    UpdateExpression: "set title = :t, description = :d",
+    ExpressionAttributeValues: {
+      ":t": title,
+      ":d": description
+    },
+    ReturnValues: "UPDATED_NEW"
   };
+
+  try {
+    const data = await dynamoDB.update(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Todo updated successfully",
+          updatedItem: data.Attributes
+        },
+        null,
+        2
+      ),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          message: "Cannot update todo"
+        },
+        null,
+        2
+      ),
+    };
+  }
 };
